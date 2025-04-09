ff6741e64d4477b2d6ad776b0853e701ff78b5ef
suleman37 <sulemanshakeel37@gmail.com>
2025-04-09 12:20:36 +0500

    Creating Seperate functions

--- main.go
+++ main.go
@@ -11,82 +11,129 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	a := string(file)
+	fileContent := string(file)
 
-	fmt.Println("Total Number of Characters :", len(a))
-
+	fmt.Println("Total Number of Characters :", len(fileContent))
 	// Add Number of Words Logic
-	words := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] == ' ' {
-			words++
-		}
-	}
+	words := findWordCount(fileContent)
 	fmt.Println("Total Number of words      :", words)
 
 	// Add Number of Lines Logic
-	lines := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] == '\n' {
-			lines++
-		}
-	}
+	lines := findlinesCount(fileContent)
 	fmt.Println("Total Number of Lines      :", lines)
 
 	// Add Number of Spaces Logic
+	spaces := findspacesCount(fileContent)
+	fmt.Println("Total Number of Spaces      :", spaces-1)
+
+	// Add Number of vowels Logic
+	vowels := findvowelsCount(fileContent)
+	fmt.Println("Total Number of Vowels     :", vowels)
+
+	// Add Number of Punctuations Logic
+	punchar := findPunctuationsCount(fileContent)
+	fmt.Println("Total Number of Punctuations:", punchar)
+
+	// Add Number of Digits Logic
+	digits := findDigitsCount(fileContent)
+	fmt.Println("Total Number of Digits   :", digits)
+
+	// Add Number of Digits Logic
+	cons := findConsonentsCount(fileContent)
+	fmt.Println("Total Number of Vowels     :", cons)
+
+	// Add Number of Special Characters Logic
+	special := findspecialCount(fileContent)
+	fmt.Println("Total Number of Special Characters    :", special)
+}
+
+// Seperate Funcation for words count
+func findWordCount(fileContent string) int {
+	words := 0
+	for i := 0; i < len(fileContent); i++ {
+		if fileContent[i] == ' ' {
+			words++
+		}
+	}
+
+	return words
+}
+
+// Seperate Funcation for lines count
+func findlinesCount(fileContent string) int {
+	lines := 0
+	for i := 0; i < len(fileContent); i++ {
+		if fileContent[i] == '\n' {
+			lines++
+		}
+	}
+	return lines
+}
+
+// Seperate Funcation for Spaces count
+func findspacesCount(fileContent string) int {
 	spaces := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] == ' ' {
+	for i := 0; i < len(fileContent); i++ {
+		if fileContent[i] == ' ' {
 			spaces++
 		}
 	}
-	fmt.Println("Total Number of Spaces     :", spaces-1)
+	return spaces
+}
 
-	// Add Number of vowels Logic
+// Seperate Funcation for Spaces count
+func findvowelsCount(fileContent string) int {
 	vowels := 0
-	for i := 0; i < len(a); i++ {
-		switch a[i] {
+	for i := 0; i < len(fileContent); i++ {
+		switch fileContent[i] {
 		case 'a', 'e', 'i', 'o', 'u':
 			vowels++
 		}
 	}
-	fmt.Println("Total Number of Vowels     :", vowels)
+	return vowels
+}
 
-	// Add Number of Punctuations Logic
+// Seperate Funcation for Punctuations count
+func findPunctuationsCount(fileContent string) int {
 	punchar := 0
-	for i := 0; i < len(a); i++ {
-		switch a[i] {
+	for i := 0; i < len(fileContent); i++ {
+		switch fileContent[i] {
 		case '.', ',', '\'', '(', ')', '!', '?', ';', ':', '-', '"':
 			punchar++
 		}
 	}
-	fmt.Println("Total Number of Punctuations:", punchar)
+	return punchar
+}
 
-	// Add Number of Digits Logic
+// Seperate Funcation for Digits count
+func findDigitsCount(fileContent string) int {
 	digits := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] >= '0' && a[i] <= '9' {
+	for i := 0; i < len(fileContent); i++ {
+		if fileContent[i] >= '0' && fileContent[i] <= '9' {
 			digits++
 		}
 	}
-	fmt.Println("Total Number of Digits   :", digits)
+	return digits
+}
 
-	// Add Number of consonents Logic
+// Seperate Funcation for Consonents count
+func findConsonentsCount(fileContent string) int {
 	cons := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] != 'a' && a[i] != 'e' && a[i] != 'i' && a[i] != 'o' && a[i] != 'u' {
+	for i := 0; i < len(fileContent); i++ {
+		if fileContent[i] != 'a' && fileContent[i] != 'e' && fileContent[i] != 'i' && fileContent[i] != 'o' && fileContent[i] != 'u' {
 			cons++
 		}
 	}
-	fmt.Println("Total Number of Vowels     :", cons)
+	return cons
+}
 
-
-	// Add Number of Special Characters Logic
+// Seperate Funcation for Special Characters count
+func findspecialCount(fileContent string) int {
 	special := 0
-	for i := 0; i < len(a); i++ {
-			if !(a[i] >= 'a' && a[i] <= 'z') && !(a[i] >= 'A' && a[i] <= 'Z') && !(a[i] >= '0' && a[i] <= '9') && a[i] != ' ' {
-				special++
-			}
+	for i := 0; i < len(fileContent); i++ {
+		if !(fileContent[i] >= 'a' && fileContent[i] <= 'z') && !(fileContent[i] >= 'A' && fileContent[i] <= 'Z') && !(fileContent[i] >= '0' && fileContent[i] <= '9') && fileContent[i] != ' ' {
+			special++
+		}
 	}
-	fmt.Println("Total Number of Special Characters    :", special)
+	return special
 }
