4a26922fd5dad0480b5fe579b7c498312a0d56ff
suleman37 <sulemanshakeel37@gmail.com>
2025-04-10 17:27:05 +0500

    chunking

--- analyzer/Combination.go
+++ analyzer/Combination.go
@@ -9,10 +9,8 @@ func Combination(fileContent string) (int, int, int, int, int, int, int, int) {
 	line := 0
 	cons := 0
 	digits := 0
-
 	for i := 0; i < len(fileContent); i++ {
 		char := fileContent[i]
-
 		if char == ' ' {
 			words++
 			spaces++
@@ -20,7 +18,6 @@ func Combination(fileContent string) (int, int, int, int, int, int, int, int) {
 		if char == '\n' {
 			line++
 		}
-
 		switch char {
 		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
 			vowels++
@@ -29,16 +26,13 @@ func Combination(fileContent string) (int, int, int, int, int, int, int, int) {
 				cons++
 			}
 		}
-
 		switch char {
 		case '.', ',', '\'', '(', ')', '!', '?', ';', ':', '-', '"':
 			punchar++
 		}
-
 		if char >= '0' && char <= '9' {
 			digits++
 		}
-
 		if !(char >= 'a' && char <= 'z') &&
 			!(char >= 'A' && char <= 'Z') &&
 			!(char >= '0' && char <= '9') &&
@@ -46,6 +40,5 @@ func Combination(fileContent string) (int, int, int, int, int, int, int, int) {
 			special++
 		}
 	}
-
 	return words, line, spaces, vowels, punchar, digits, cons, special
 }
--- main.go
+++ main.go
@@ -2,32 +2,45 @@ package main
 
 import (
 	"fmt"
+	"gotask/analyzer"
 	"os"
 	"time"
-	"gotask/analyzer"
 )
 
 func main() {
-
 	now := time.Now()
-
 	file, err := os.ReadFile("text.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fileContent := string(file)
+	lengthofData := len(fileContent);
 
-	fmt.Println("Total Number of Characters :", len(fileContent))
+	chunksize:=lengthofData / 3
+
+	fmt.Println(chunksize)
+	chunks := make([]string,8);
+	for i := 0; i < 8; i++ {
+		startChunk := i * chunksize;
+		endChunk := (i + 1) * chunksize;
+		if i == 8-1 {
+			endChunk = lengthofData
+		}
+		chunks[i] = fileContent[startChunk:endChunk]
+	}
+
+
+	fmt.Println("Total Number of Characters  :", len(fileContent))
 	words, line, spaces, vowels, punchar, digits, cons, special := analyzer.Combination(fileContent)
 	fmt.Println("Total Number of words       :", words)
 	fmt.Println("Total Number of Lines       :", line)
 	fmt.Println("Total Number of Spaces      :", spaces-1)
 	fmt.Println("Total Number of Vowels      :", vowels)
 	fmt.Println("Total Number of Punctuations:", punchar)
-	fmt.Println("Total Number of Digits   :", digits)
-	fmt.Println("Total Number of Vowels     :", cons)
-	fmt.Println("Total Number of Special Characters    :", special)
+	fmt.Println("Total Number of Digits      :", digits)
+	fmt.Println("Total Number of Vowels      :", cons)
+	fmt.Println("Total Special Characters    :", special)
 	timeTaken := time.Since(now)
-	fmt.Println("Time taken in whole processing: ", timeTaken)
+	fmt.Println("Time taken in whole processing:", timeTaken)
 }
