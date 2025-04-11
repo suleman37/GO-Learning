dd1eeb82cd2d99eb8de683c82a161e31cb895802
suleman37 <sulemanshakeel37@gmail.com>
2025-04-11 15:19:05 +0500

    Post data method

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
-	ID     int    `json:"id"`
+	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Model string `json:"model"`
 }
@@ -15,7 +16,7 @@ func main() {
 	r := gin.Default()
 
 	books := []Book{
-		{ID: 1, Name: "Toyota Raiz", Model: "2024"},
+		{ID: 1, Name: "Toyota Raize", Model: "2024"},
 		{ID: 2, Name: "Grandee X", Model: "2025"},
 	}
 
@@ -23,5 +24,10 @@ func main() {
 		c.JSON(http.StatusOK, books)
 	})
 
+	r.POST("/post", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Data Posted Sucessfully!",
+		})
+	})
 	r.Run(":8080")
 }
